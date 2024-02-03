934d6f438eb7778d1636d0ae18f2152dca6095c6
Yasir Eymen Kayabaşı <yasirator04@gmail.com>
2024-02-03 17:36:34 +0300

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -54,6 +54,7 @@ curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/less.bat
 curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/mv.bat
 curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/pwd.bat
 curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/sed.bat
+curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/sudo.bat
 curl -O https://raw.githubusercontent.com/srcatarr/xInWin/main/commands/xiw.bat
 
 cd %wd%
